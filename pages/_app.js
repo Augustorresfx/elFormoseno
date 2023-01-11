4225e2c79ad03eb82a486eb27d5aea588b7296e0
Augustorresfx <augustorresfx@gmail.com>
2023-01-11 18:50:59 -0300

    adding-google-analytics-4

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { ParallaxProvider } from 'react-scroll-parallax'
 // 1. Import the extendTheme function
 import { extendTheme } from '@chakra-ui/react'
+import { useEffect } from 'react'
+import { init } from '../utils/ga'
 
 // 2. Extend the theme to include custom colors, fonts, etc
 const colors = {
@@ -21,6 +23,10 @@ const theme = extendTheme({ colors,
 
 // 3. Pass the `theme` prop to the `ChakraProvider`
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    init(process.env.NEXT_PUBLIC_G);
+  }, []);
+  
   return (
     <ParallaxProvider scrollAxis='vertical'>
     <ChakraProvider theme={theme}>
