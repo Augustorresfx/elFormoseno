c71f734c866c3bfe47e8d51d5c821aab93c4b048
Augustorresfx <augustorresfx@gmail.com>
2023-01-11 19:02:45 -0300

    added-head-script-ga4

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,22 @@ export default function Document() {
   return (
     <Html lang='es'>
       <Head />
+      <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_G}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_G}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       <title>El Formoseño</title>
       <link href='https://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet'></link>
       <body>
